c7cec73f59098cb53f2acb0acab8883fe3390c71
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2025-05-13 23:23:17 +0100

    Complete Week 1 task

---
+++ HelloWorldPrg.java
@@ -0,0 +1,11 @@
+// An 'Hello World' program that prints 'Hello World' out to the console.
+
+/** 
+ * main - prints 'Hello World' out to console
+ * @args: command line arguments
+*/
+class HelloWorldPrg {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
